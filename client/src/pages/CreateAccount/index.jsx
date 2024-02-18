97713fdc4dc3909469eda3605c338b687e418e6c
joyce-luiza <joyceluizabrito@gmail.com>
2024-02-18 17:48:13 -0300

    updating create account page

--- client/src/pages/CreateAccount/index.jsx
+++ client/src/pages/CreateAccount/index.jsx
@@ -1,132 +1,247 @@
 import React, { useState } from "react";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Carousel, Radio, message } from "antd";
 import styles from "./styles.module.css";
 import MaskedInput from "../../components/MaskedInput";
-import DatePicker from "../../components/DatePicker";
+import testimonialImage1 from "./images/image-1.jpg";
+import PasswordField from "../../components/PasswordField";
+import { axiosRequest } from "../../utils/axiosRequest";
+import BirthDateField from "../../components/BirthDateField";
 
 export default function CreateAccount() {
-    const [userProfile, setUserProfile] = useState({
-        nomeCompleto: "",
-        email: "",
-        numeroCelular: "",
-        dataNascimento: "",
-        senha: "",
-    });
+    const [accountType, setAccountType] = useState("adopter");
+    const [messageApi, contextHolder] = message.useMessage();
+    const [loading, setLoading] = useState(false);
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setUserProfile((prevProfile) => ({
-            ...prevProfile,
-            [name]: value,
-        }));
+    const changeAccountType = (event) => {
+        setAccountType(event.target.value);
     };
 
-    const handleSubmit = () => {
-        console.log({ ...userProfile });
+    const successMessage = () => {
+        messageApi.open({
+            type: "success",
+            content: "Conta criada com sucesso!",
+            duration: 5,
+        });
     };
 
-    const [passwordVisible, setPasswordVisible] = React.useState(false);
+    const errorMessage = () => {
+        messageApi.open({
+            type: "error",
+            content: "Erro ao criar conta.",
+            duration: 5,
+        });
+    };
+
+    const handleSubmit = async ({
+        fullName,
+        birthDate,
+        password,
+        phoneNumber,
+        email,
+    }) => {
+        setLoading(true);
+        const firstName = fullName.split(" ")[0];
+        const lastName = fullName.slice(firstName.length).trim();
+        const body = {
+            firstName: firstName,
+            lastName: lastName,
+            birthDate: birthDate,
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber,
+        };
+
+        const result = await axiosRequest({
+            method: "post",
+            path: "/adopter",
+            body,
+        });
+
+        if (result) {
+            setLoading(false);
+            return successMessage();
+        } else {
+            setLoading(false);
+            return errorMessage();
+        }
+    };
 
     return (
-        <div className={styles.container}>
-            <section className={styles.createAccount}>
-                <h2 className={styles.title}>Criar conta</h2>
-                <Form
-                    layout="vertical"
-                    style={{ width: "70%" }}
-                    initialValues={userProfile}
-                    onFinish={handleSubmit}
-                >
-                    <Form.Item
-                        label="Nome completo:"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Insira seu nome completo.",
-                            },
-                        ]}
-                    >
-                        <Input
-                            name={"nomeCompleto"}
-                            size="large"
-                            placeholder="Nome completo"
-                            value={userProfile.nomeCompleto}
-                            onChange={handleChange}
-                        ></Input>
-                    </Form.Item>
-                    <Form.Item
-                        label="Email:"
-                        rules={[
-                            {
-                                type: "email",
-                                message:
-                                    "O email inserido não possui um formato válido.",
-                            },
-                            {
-                                required: true,
-                                message: "Insira um endereço de email",
-                            },
-                        ]}
-                    >
-                        <Input
-                            name={"email"}
-                            size="large"
-                            placeholder="Email"
-                            value={userProfile.email}
-                            onChange={handleChange}
-                        ></Input>
-                    </Form.Item>
-                    <Form.Item label="Número de celular:">
-                        <MaskedInput
-                            name={"numeroCelular"}
-                            value={userProfile.numeroCelular}
-                            onChange={handleChange}
-                            type="text"
-                            mask="(99) 99999-9999"
-                            placeholder="(00) 00000-0000"
-                        ></MaskedInput>
-                    </Form.Item>
-                    <Form.Item label="Data de nascimento:">
-                        <DatePicker
-                            name="dataNascimento"
-                            selected={userProfile.dataNascimento}
-                            handleChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        label="Senha:"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Insira uma senha.",
-                            },
-                        ]}
-                    >
-                        <Input.Password
-                            size="large"
-                            placeholder="Senha"
-                            visibilityToggle={{
-                                visible: passwordVisible,
-                                onVisibleChange: setPasswordVisible,
+        <>
+            {contextHolder}
+            <div className={styles.container}>
+                <section className={styles.createAccount}>
+                    <h2 className={styles.title}>Criar conta</h2>
+
+                    {accountType === "adopter" && (
+                        <Form
+                            layout="vertical"
+                            style={{ width: "70%" }}
+                            initialValues={{
+                                fullName: "",
+                                email: "",
+                                phoneNumber: "",
+                                birthDate: "",
+                                password: "",
                             }}
-                            name={"senha"}
-                            value={userProfile.senha}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button
-                            block
-                            size="large"
-                            type="primary"
-                            htmlType="submit"
+                            onFinish={handleSubmit}
                         >
-                            Criar conta
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </section>
-            <section className={styles.banner}></section>
-        </div>
+                            <Form.Item>
+                                <div className={styles.accountType}>
+                                    <h3>Tipo de conta</h3>
+                                    <Radio.Group
+                                        onChange={changeAccountType}
+                                        value={accountType}
+                                    >
+                                        <Radio value={"adopter"}>
+                                            Sou adotante
+                                        </Radio>
+                                        <Radio value={"organization"}>
+                                            Sou uma organização
+                                        </Radio>
+                                    </Radio.Group>
+                                </div>
+                            </Form.Item>
+                            <Form.Item
+                                name={"fullName"}
+                                label="Nome completo:"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Insira o seu nome completo",
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    size="large"
+                                    placeholder="Nome completo"
+                                ></Input>
+                            </Form.Item>
+                            <Form.Item
+                                name={"email"}
+                                label="Email:"
+                                rules={[
+                                    {
+                                        type: "email",
+                                        message:
+                                            "O email inserido não possui um formato válido.",
+                                    },
+                                    {
+                                        required: true,
+                                        message: "Insira um endereço de email",
+                                    },
+                                ]}
+                                validateTrigger="onBlur"
+                            >
+                                <Input size="large" placeholder="Email"></Input>
+                            </Form.Item>
+                            <MaskedInput
+                                name={"phoneNumber"}
+                                label="Número de celular:"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Insira um número de celular",
+                                    },
+                                ]}
+                                type="text"
+                                mask="(99) 99999-9999"
+                                placeholder="(00) 00000-0000"
+                            ></MaskedInput>
+                            {/* <DatePicker
+                                name={"birthDate"}
+                                label="Data de nascimento:"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message:
+                                            "Insira a sua data de nascimento",
+                                    },
+                                ]}
+                            /> */}
+                            <BirthDateField
+                                name={"birthDate"}
+                                label={"Data de nascimento"}
+                            ></BirthDateField>
+                            <PasswordField name={"password"}></PasswordField>
+                            <Form.Item>
+                                <Button
+                                    block
+                                    size="large"
+                                    type="primary"
+                                    htmlType="submit"
+                                    loading={loading}
+                                >
+                                    Criar conta
+                                </Button>
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="link" style={{ width: "100%" }}>
+                                    Já possui conta? Faça login
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    )}
+                    {accountType === "organization" && (
+                        <Form
+                            layout="vertical"
+                            style={{ width: "70%" }}
+                            initialValues={""}
+                        >
+                            <Form.Item>
+                                <div className={styles.accountType}>
+                                    <h3>Tipo de conta</h3>
+                                    <Radio.Group
+                                        onChange={changeAccountType}
+                                        value={accountType}
+                                    >
+                                        <Radio value={"adopter"}>
+                                            Sou adotante
+                                        </Radio>
+                                        <Radio value={"organization"}>
+                                            Sou uma organização
+                                        </Radio>
+                                    </Radio.Group>
+                                </div>
+                            </Form.Item>
+                            <h3>Confia que aqui vai ter um formulário</h3>
+                        </Form>
+                    )}
+                </section>
+                <div style={{ position: "fixed", right: "0" }}>
+                    <Carousel
+                        className={styles.carousel}
+                        // autoplay
+                    >
+                        <div className={styles.banner}>
+                            <div
+                                style={{
+                                    backgroundImage: `url(${testimonialImage1})`,
+                                }}
+                                className={styles.backgroundPhoto}
+                            ></div>
+                            <p className={styles.testimonialText}>
+                                "Encontrei Teodoro, meu fiel amigo de quatro
+                                patas, no site! Ele trouxe alegria e amor para
+                                nossa casa. Obrigado, [nome do site]!"
+                            </p>
+                            <div className={styles.testimonialAuthor}>
+                                <div></div>
+                                <p>Maiara, tutora do Teodoro</p>
+                            </div>
+                        </div>
+                        <div className={styles.banner}>
+                            <h3>2</h3>
+                        </div>
+                        <div>
+                            <h3>3</h3>
+                        </div>
+                        <div>
+                            <h3>4</h3>
+                        </div>
+                    </Carousel>
+                </div>
+            </div>
+        </>
     );
 }
