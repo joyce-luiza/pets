afea55e64ccb96cba4171ac66da324c447c59373
joyce-luiza <joyceluizabrito@gmail.com>
2024-03-05 20:58:19 -0300

    removing console.log

--- client/src/contexts/ProtectedRoute.js
+++ client/src/contexts/ProtectedRoute.js
@@ -4,16 +4,8 @@ import { useAuth } from "./AuthContext";
 
 const ProtectedRoute = () => {
     const { user } = useAuth();
-
-    console.log("1. o user é" + user + " com o tipo " + typeof user);
-    const outroUser = JSON.parse(user);
-    console.log("2. o outro user é" + outroUser);
-    console.log(
-        "2. o outro user é" + outroUser + " com o tipo " + typeof outroUser
-    );
-    console.log("3. o token é: " + outroUser?.token);
-
-    return outroUser && outroUser.token ? <Outlet /> : <Navigate to="/login" />;
+    const userData = JSON.parse(user);
+    return userData && userData.token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
