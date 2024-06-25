61d315fb1d2ae32e601e2748d8808453083212d6
Marcelle Viana <marcellevianadasneves@gmail.com>
2024-06-25 13:25:57 -0300

    resolving conflicts

--- server/src/app/repositories/AdoptionRepository.js
+++ server/src/app/repositories/AdoptionRepository.js
@@ -1,63 +1,71 @@
-import { Adoption } from "../../database/models";
-import AbstractRepository from "../abstract/AbstractRepository";
+import { Adoption } from '../../database/models';
+import AbstractRepository from '../abstract/AbstractRepository';
 
 class AdoptionRepository extends AbstractRepository {
-    constructor() {
-        super(Adoption);
-        this.createAdoption = this.createAdoption.bind(this);
-        this.findAllToTableView = this.findAllToTableView.bind(this);
+  constructor() {
+    super(Adoption);
+    this.createAdoption = this.createAdoption.bind(this);
+    this.findAllToTableView = this.findAllToTableView.bind(this);
+  }
+
+  async createAdoption({ adopterId, animalId, notes }) {
+    return await this.create({
+      adopterId,
+      animalId,
+      notes,
+      organizationReply: '',
+      resultId: await this.getPedingResultId(),
+      statusId: await this.getActiveStatusId(),
+    });
+  }
+
+  async findActiveAdoptionById(id) {
+    return await this.findOne({
+      where: {
+        id,
+        statusId: await this.getActiveStatusId(),
+      },
+    });
+  }
+
+  async findAllToTableView({ page, size, isPaginated, organizationId }) {
+    let adoptionQuery = `
+      SELECT
+        ad.id,
+        an.name AS animalName,
+        at.title AS "type",
+        adop.id AS adopterId,
+        adop.first_name AS adopterFirstName,
+        adop.last_name AS adopterLastName,
+        adop.email AS adopterEmail,
+        r.title AS result,
+        s.description AS status,
+        ad.created_at AS "createdAt"
+      FROM
+        "Adoptions" ad
+      INNER JOIN "Statuses" s ON ad.status_id = s.id
+      INNER JOIN "Results" r ON ad.result_id = r.id
+      INNER JOIN "Adopters" adop ON ad.adopter_id = adop.id
+      INNER JOIN "Animals" an ON ad.animal_id = an.id
+      INNER JOIN "AnimalTypes" at ON at.id = an.type_id
+    `;
+    const replacements = {};
+
+    if (organizationId) {
+      adoptionQuery += `WHERE an.organization_id = :organizationId `;
+      replacements.organizationId = `${organizationId}`;
     }
 
-    async createAdoption({ adopterId, animalId, notes }) {
-        return await this.create({
-            adopterId,
-            animalId,
-            notes,
-            organizationReply: "",
-            resultId: await this.getPedingResultId(),
-            statusId: await this.getActiveStatusId(),
-        });
-    }
+    const adoptions = await this.paginateSqlQuery(
+      adoptionQuery,
+      page,
+      size,
+      isPaginated,
+      replacements
+    );
 
-    async findActiveAdoptionById(id) {
-        return await this.findOne({
-            where: {
-                id,
-                statusId: await this.getActiveStatusId(),
-            },
-        });
-    }
-
-    async findAllToTableView({ page, size, isPaginated }) {
-        const adoptionQuery = `
-            SELECT
-                ad.id,
-                an.name AS animalName,
-                at.title AS "type",
-                adop.id AS adopterId,
-                adop.first_name AS adopterFirstName,
-                adop.last_name AS adopterLastName,
-                adop.email AS adopterEmail,
-                r.title AS result,
-                s.description AS status,
-                ad.created_at AS "createdAt"
-            FROM
-                "Adoptions" ad
-            INNER JOIN "Statuses" s ON ad.status_id = s.id
-            INNER JOIN "Results" r ON ad.result_id = r.id
-            INNER JOIN "Adopters" adop ON ad.adopter_id = adop.id
-            INNER JOIN "Animals" an ON ad.animal_id = an.id
-            INNER JOIN "AnimalTypes" at ON at.id = an.type_id
-        `;
-
-        const adoptions = await this.paginateSqlQuery(
-            adoptionQuery,
-            page,
-            size,
-            isPaginated
-        );
-        return adoptions;
-    }
+    return adoptions;
+  }
 }
 
 export default new AdoptionRepository();
--- server/src/routes/Adoptions/strategies/FindAllToTableViewStrategy.js
+++ server/src/routes/Adoptions/strategies/FindAllToTableViewStrategy.js
@@ -1,6 +1,6 @@
-import AbstractStrategy from "../../../app/abstract/AbstractStrategy";
-import { AdoptionAdapter, Pagination } from "../../../app/domains/adapters";
-import { Adoption } from "../../../app/domains";
+import AbstractStrategy from '../../../app/abstract/AbstractStrategy';
+import { AdoptionAdapter, Pagination } from '../../../app/domains/adapters';
+import { Adoption } from '../../../app/domains';
 
 /**
  * Strategy to list all adoptions in a table
@@ -8,34 +8,36 @@ import { Adoption } from "../../../app/domains";
  * @extends AbstractStrategy
  */
 export default class FindAllToTableViewStrategy extends AbstractStrategy {
-    constructor(adoptionRepository) {
-        super();
-        this.adoptionRepository = adoptionRepository;
+  constructor(adoptionRepository) {
+    super();
+    this.adoptionRepository = adoptionRepository;
+  }
+
+  /**
+   * @param {Pagination} data - Pagination filter object
+   */
+  async execute(data, dto) {
+    let result = [];
+
+    // Log the conditions for debugging
+    const { ...paginationData } = data;
+
+    const adoptions = await this.adoptionRepository.findAllToTableView({
+      ...paginationData,
+      organizationId: data.conditions.organizationId,
+    });
+
+    if (adoptions.length) {
+      result = adoptions.map((adoption) => new AdoptionAdapter(adoption));
     }
 
-    /**
-     * @param {Pagination} data - Pagination filter object
-     */
-    async execute(data, dto) {
-        let result = [];
-
-        const adoptions = await this.adoptionRepository.findAllToTableView(
-            data
-        );
-
-        if (adoptions.length) {
-            result = adoptions.map((adoption) => new AdoptionAdapter(adoption));
-        }
-
-        if ("data" in adoptions && adoptions.data.length) {
-            result = {
-                ...adoptions,
-                data: adoptions.data.map(
-                    (adoption) => new AdoptionAdapter(adoption)
-                ),
-            };
-        }
-
-        return result;
+    if (adoptions.data && adoptions.data.length) {
+      result = {
+        ...adoptions,
+        data: adoptions.data.map((adoption) => new AdoptionAdapter(adoption)),
+      };
     }
+
+    return result;
+  }
 }
